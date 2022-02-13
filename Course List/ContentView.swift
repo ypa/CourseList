8e04f2cdb2288e292ea791a0caf6bc9b3b4c6e8f
Yan P Aung <yan.pye.aung@gmail.com>
2022-02-12 20:30:21 -0800

    Initial Commit

---
+++ Course List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course List
+//
+//  Created by YAN AUNG on 2/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Course List/Course_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ListApp.swift
+//  Course List
+//
+//  Created by YAN AUNG on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Course_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
