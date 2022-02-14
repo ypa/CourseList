0a3087c5d99d6285b58c6b75b4f76b2490aba3cf
Yan P Aung <yan.pye.aung@gmail.com>
2022-02-13 19:40:33 -0800

    Fetch data from API

--- Course List/ContentView.swift
+++ Course List/ContentView.swift
@@ -10,7 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onAppear(perform: {
+                getCourses()
+            })
+    }
+    
+    func getCourses() {
+        // https://zappycode.com/api/courses?format=json
+        if let apiUrl = URL(string: "https://zappycode.com/api/courses?format=json") {
+            var request = URLRequest(url: apiUrl)
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    print("There was an error")
+                } else if data != nil {
+                    print(String(data: data!, encoding: .utf8) ?? "Error")
+                }
+            }.resume()
+        }
     }
 }
 
